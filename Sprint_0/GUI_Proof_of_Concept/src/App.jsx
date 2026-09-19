4002286d01b95d8c7e035f7ef28c86bae12d5869
Derek Ashdown <daamqm@umsystem.edu>
2026-09-18 21:48:23 -0500

    gui

--- Sprint_0/GUI_Proof_of_Concept/src/App.jsx
+++ Sprint_0/GUI_Proof_of_Concept/src/App.jsx
@@ -1,122 +1,61 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
+  const [hints, setHints] = useState(false);
+  const [choice, setChoice] = useState("");
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+    <main>
+      <h1>Testing</h1>
+      <hr />
+      <p>This is a simple test of the GUI.</p>
+      <svg width="200" height="10">
+        <line x1="0" y1="5" x2="200" y2="5" stroke="currentColor" />
+      </svg>
+      <div>
+          <label>
+          <input
+            type="checkbox"
+            checked={hints}
+            onChange={(e) => setHints(e.target.checked)}
+          />
+          Test Button
+        </label>
+      </div>
+      <div>
+        <fieldset>
+          <legend>Radio Buttons</legend>
+          <label>
+            <input
+              type="radio"
+              name="Pick ME!"
+              value="Pick ME!"
+              checked={choice === "Pick ME!"}
+              onChange={(e) => setChoice(e.target.value)}
+            />
+            Pick Me!
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="No, ME!"
+              value="No, ME!"
+              checked={choice === "No, ME!"}
+              onChange={(e) => setChoice(e.target.value)}
+            />
+            No, ME!
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="Actually, ME!"
+              value="Actually, ME!"
+              checked={choice === "Actually, ME!"}
+              onChange={(e) => setChoice(e.target.value)}
+            />
+            Actually, ME!
+          </label>
+        </fieldset>
+      </div>
+    </main>
+  );
 }
-
-export default App
